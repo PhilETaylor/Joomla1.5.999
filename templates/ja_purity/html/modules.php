36f2d7dae79a83693cb565b57854d0390767bc9c
Ricardo <ric.valechat@gmail.com>
2016-01-19 21:17:41 -0200

    CVE-2012-2413: Use fix strategy suggested by Joey3000

--- templates/ja_purity/html/modules.php
+++ templates/ja_purity/html/modules.php
@@ -52,10 +52,12 @@ function modChrome_jarounded($module, &$params, &$attribs)
 					<div>
 						<?php if ($module->showtitle != 0) : ?>
 						<?php
-						if(isset($_COOKIE['Mod'.$module->id])) $modhide = $_COOKIE['Mod'.$module->id];
-						else $modhide = 'show';
+						if (!empty($_COOKIE['Mod'.$module->id]) && ($_COOKIE['Mod'.$module->id] === 'hide'))
+							$modhide = 'hide';
+						else
+							$modhide = 'show';
 						?>
-						<h3 class="<?php echo htmlspecialchars($modhide, ENT_QUOTES, 'UTF-8'); ?>"><span><?php echo $module->title; ?></span></h3>
+						<h3 class="<?php echo $modhide; ?>"><span><?php echo $module->title; ?></span></h3>
 						<?php endif; ?>
 						<div class="jamod-content"><?php echo $module->content; ?></div>
 					</div>
